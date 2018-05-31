d708cde50cb7d44c74f93431f692c94838808d50
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-31 11:22:20 -0300

    update angula 4.3

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from "@angular/core";
 import { ShoppingCartServices } from "../restaurant-detail/shopping-cart/shopping-cart.services";
 import { CartItem } from "../restaurant-detail/shopping-cart/cart-item.model";
 import { Order } from "./order.model";
-import { Http, Headers, RequestOptions } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { MEAT_API } from "../app.api";
 
 @Injectable()
 export class OrderService {
-    constructor(private cartService: ShoppingCartServices, private http: Http){}
+    constructor(private cartService: ShoppingCartServices, private http: HttpClient){}
 
     cartItens(): CartItem[]{
         return this.cartService.items;
@@ -32,14 +32,7 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/json');        
-        
-        return this.http.post(`${MEAT_API}/orders`, 
-                             JSON.stringify(order), 
-                             new RequestOptions({headers: headers}))
-                .map(response=> response.json())
-                .map(order => order.id);
+        return this.http.post<Order>(`${MEAT_API}/orders`, order).map(order => order.id);
     }
 
     clear() {
