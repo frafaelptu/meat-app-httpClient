6d93c49ebf53fca2556f1e0c85a1ca9c93fcbc2a
frafaelptu <frafaelptu@yahoo.com.br>
2018-05-31 18:00:50 -0300

    token JWT backend

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import { Express } from 'express';
 import * as fs from 'fs';
 import * as https from 'https';
 import { handleAuthentication } from './auth'
+import { handleAuthorization } from './authz';
 
 
 
@@ -19,6 +20,7 @@ server.use(jsonServer.bodyParser);
 
 //middleware para login
 server.post('/login', handleAuthentication);
+server.use('/orders', handleAuthorization)
 
 // Use default router
 server.use(router);
