c856d61d468888022eeede8dbd38eb2c81e262c4
frafaelptu <frafaelptu@yahoo.com.br>
2018-06-01 16:54:24 -0300

    canActivate
    Regras de navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -21,7 +21,7 @@ export const ROUTES: Routes = [
             {path: 'reviews', component: ReviewsComponent},
         ]},
     {path: 'order', loadChildren: './order/order.module#OrderModule',
-        canLoad: [LoggedInGuard]},
+        canLoad: [LoggedInGuard], canActivate: [LoggedInGuard]},
     {path: 'order-summary', component: OrderSummaryComponent},
     {path: 'about', loadChildren: './about/about.module#AboutModule'},
     //Rota de WildCard - coringa
